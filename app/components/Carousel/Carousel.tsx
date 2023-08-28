280d41641b0191cabc2e8d229e264be95e236358
Le Quang Dong <dong.lq@hodfords>
2023-08-28 16:30:53 +0700

    feat: finish basic layout

---
+++ app/components/Carousel/Carousel.tsx
@@ -0,0 +1,222 @@
+"use client";
+import React, { Fragment, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import Image from "next/image";
+import type { Swiper as SwiperType } from "swiper";
+const Carousel = () => {
+  const swiperRef = useRef<SwiperType>();
+  const products = [
+    {
+      id: 1,
+      name: "Earthen Bottle",
+      href: "#",
+      price: "$48",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+      imageAlt:
+        "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+    },
+    {
+      id: 2,
+      name: "Nomad Tumbler",
+      href: "#",
+      price: "$35",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+      imageAlt:
+        "Olive drab green insulated bottle with flared screw lid and flat top.",
+    },
+    {
+      id: 3,
+      name: "Focus Paper Refill",
+      href: "#",
+      price: "$89",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+      imageAlt:
+        "Person using a pen to cross a task off a productivity paper card.",
+    },
+    {
+      id: 4,
+      name: "Machined Mechanical Pencil",
+      href: "#",
+      price: "$35",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+      imageAlt:
+        "Hand holding black machined steel mechanical pencil with brass tip and top.",
+    },
+    {
+      id: 12,
+      name: "Earthen Bottle",
+      href: "#",
+      price: "$48",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+      imageAlt:
+        "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+    },
+    {
+      id: 42,
+      name: "Nomad Tumbler",
+      href: "#",
+      price: "$35",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+      imageAlt:
+        "Olive drab green insulated bottle with flared screw lid and flat top.",
+    },
+    {
+      id: 34,
+      name: "Focus Paper Refill",
+      href: "#",
+      price: "$89",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+      imageAlt:
+        "Person using a pen to cross a task off a productivity paper card.",
+    },
+    {
+      id: 45,
+      name: "Mechanical Pencil",
+      href: "#",
+      price: "$35",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+      imageAlt:
+        "Hand holding black machined steel mechanical pencil with brass tip and top.",
+    },
+    {
+      id: 242,
+      name: "Nomad Tumbler",
+      href: "#",
+      price: "$35",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+      imageAlt:
+        "Olive drab green insulated bottle with flared screw lid and flat top.",
+    },
+    {
+      id: 33334,
+      name: "Focus Paper Refill",
+      href: "#",
+      price: "$89",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+      imageAlt:
+        "Person using a pen to cross a task off a productivity paper card.",
+    },
+    {
+      id: 4445,
+      name: "Machined Mechanical",
+      href: "#",
+      price: "$35",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+      imageAlt:
+        "Hand holding black machined steel mechanical pencil with brass tip and top.",
+    },
+    // More products...
+  ];
+  return (
+    <div className="relative mx-auto px-4 py-4 w-full lg:px-8">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper;
+        }}
+        loop
+        autoplay={{
+          delay: 500,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Autoplay]}
+        className="swiper px-4 py-4 mt-6"
+      >
+        {products.map((product) => (
+          <SwiperSlide key={product.id}>
+            <a href={product.href} className="group">
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                <Image
+                  src={product.imageSrc}
+                  alt={product.imageAlt}
+                  width={500}
+                  height={500}
+                  className="h-full w-full object-cover object-center group-hover:opacity-75"
+                />
+              </div>
+              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+              <p className="mt-1 text-lg font-medium text-gray-900">
+                {product.price}
+              </p>
+            </a>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Fragment>
+        <button
+          type="button"
+          onClick={() => swiperRef.current?.slidePrev()}
+          className="absolute z-10 left-0 top-1/2 -translate-y-full rotate-180 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center ml-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          <svg
+            className="w-4 h-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </button>
+        <button
+          type="button"
+          onClick={() => swiperRef.current?.slideNext()}
+          className="absolute z-10 right-0 top-1/2 -translate-y-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          <svg
+            className="w-4 h-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </button>
+      </Fragment>
+    </div>
+  );
+};
+
+export default Carousel;
