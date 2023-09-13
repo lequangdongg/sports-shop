4a154e0d0ea5cfebe0ca88d0a67996c09b567653
Le Quang Dong <dong.lq@hodfords>
2023-09-13 15:41:29 +0700

    feat: dynamic year

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -8,8 +8,8 @@ const Footer = () => {
           <h6
             className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
           >
-            &copy; 2023
-            <a href="https://material-tailwind.com/">HM-sports</a>. All
+            &copy; {new Date().getFullYear()}
+            <a href="https://material-tailwind.com/"> HM-sports</a>. All
             Rights Reserved.
           </h6>
           <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
