031988b0cf1b2fe69c29c53004aa57b5076be83c
Le Quang Dong <dong.lq@hodfords>
2023-09-15 17:16:22 +0700

    fix: facebook inject

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
-        <div id="fb-root"></div>
+        {/* <div id="fb-root"></div>
         <div id="fb-customer-chat" className="fb-customerchat"></div>
         <Script
           id="messenger-tag"
@@ -57,7 +57,31 @@ export default function RootLayout({
               fjs.parentNode.insertBefore(js, fjs);
             }(document, 'script', 'facebook-jssdk'));`,
           }}
-        />
+        /> */}
+        <div id="fb-root"></div>
+        <div id="fb-customer-chat" className="fb-customerchat"></div>
+        <Script strategy="lazyOnload">
+          {`
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "xxREPLACEMEWITHREALDATAxxxx");
+      chatbox.setAttribute("attribution", "biz_inbox");
+
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v15.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    `}
+        </Script>
 
         <body className={inter.className}>{children}</body>
       </html>
