0590726217cf0138b09c8fc5584bbf83530ee65f
Le Quang Dong <dong.lq@hodfords>
2023-09-15 17:10:42 +0700

    fix: facebook messenger

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,25 +30,32 @@ export default function RootLayout({
         <div id="fb-root"></div>
         <div id="fb-customer-chat" className="fb-customerchat"></div>
         <Script
+          id="messenger-tag"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "115229745014270");
-      chatbox.setAttribute("attribution", "biz_inbox");
-
-      {window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v18.0'
-        });
-      };
-
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));}`,
+            chatbox.setAttribute("page_id", "115229745014270");
+            chatbox.setAttribute("attribution", "biz_inbox");`,
+          }}
+        />
+        <Script
+          id="messenger-sdk"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v18.0'
+              });
+            };
+      
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));`,
           }}
         />
 
